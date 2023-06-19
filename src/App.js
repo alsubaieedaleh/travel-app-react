0e427693e391d2289af5527351d2af71d731136f
daleh <51839961+alsubaieedaleh@users.noreply.github.com>
2023-06-19 07:06:20 +0300

    Second Commit

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import Mount from "./Mount.png";
 import "./App.css";
 import Card from "./Card.js";
 import data from "./data.js";
-
+import Linkedin from "./linkedIn.png";
+import gitHub from "./icone-github-grise.png";
 function App() {
   const cards = data.map((item) => {
     return <Card key={item.id} {...item} />;
   });
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="brand">
-          <img src={earth} className="App-logo" alt="logo" />
-          <h4 className="Title"> My travel jornal</h4>
+    <div className="Background">
+      <div className="App">
+        <header className="App-header">
+          <div className="brand">
+            <img src={earth} className="App-logo" alt="logo" />
+            <h4 className="Title"> My travel jornal</h4>
+          </div>
+        </header>
+        <div className="Card-Container">{cards}</div>
+        <div className="App-footer">
+          {" "}
+          <a href="https://www.linkedin.com/in/daleh-alsubaiee">
+            <img src={Linkedin} />
+          </a>
+          <a href="https://github.com/alsubaieedaleh">
+            <img src={gitHub} />
+          </a>
         </div>
-      </header>
-      <div className="Card-Container">{cards}</div>
+      </div>
     </div>
   );
 }
